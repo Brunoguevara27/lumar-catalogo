3d26736a4f18768bd3b943d5ac324c0ed2d798fd
brunoguevara27 <brunoguevara22@gmail.com>
2025-08-20 19:10:20 -0300

    remove admin notice

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,12 @@ import Contact from "@/components/contact"
 import Footer from "@/components/footer"
 import Cart from "@/components/cart"
 import CartButton from "@/components/cart-button"
-import AdminNotice from "@/components/admin-notice"
 import CartNotification from "@/components/cart-notification"
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <AdminNotice />
       <CartNotification />
       <main className="flex-1">
         <Hero />
